50e0accc98a96d92d39beb31bb92cb155564c176
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-29 19:28:27 +0530

    Create rectangle_area_perimeter.py

---
+++ rectangle_area_perimeter.py
@@ -0,0 +1,10 @@
+length = int(input("ENTER THE LENGTH -> "))
+breadth = int(input("ENTER THE BREADTH -> "))
+
+area = length * breadth
+perimeter = 2 * (length + breadth)
+
+print("THE AREA OF THE RECTANGLE IS ->", area)
+print("THE PERIMETER OF THE RECTANGLE IS ->", perimeter)
+
+print("Krishna Bhatia 1/23/SET/BCS/358")
