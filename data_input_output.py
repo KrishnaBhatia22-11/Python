1f3c37b1073b11fc67e16a1a9283ba9b69cccb50
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-29 19:29:24 +0530

    Create data_input_output.py

---
+++ data_input_output.py
@@ -0,0 +1,13 @@
+a = int(input("Enter the integer value -> "))
+b = float(input("Enter the float value -> "))
+c = str(input("Enter the string value -> "))
+d = complex(input("Enter the complex value -> "))
+e = bool(input("Enter the boolean value -> "))
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print("Krishna Bhatia 1/23/SET/BCS/358")
