7fd7720361317c288fb245901372eeaaca869c17
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-29 19:23:50 +0530

    Create piggybank_total.py

---
+++ piggybank_total.py
@@ -0,0 +1,7 @@
+coins_10 = int(input("Enter the number of Rs 10 coins: "))
+coins_5 = int(input("Enter the number of Rs 5 coins: "))
+coins_2 = int(input("Enter the number of Rs 2 coins: "))
+coins_1 = int(input("Enter the number of Re 1 coins: "))
+total_amount = (coins_10 * 10) + (coins_5 * 5) + (coins_2 * 2) + coins_1
+print("Total amount in the piggybank:", total_amount)
+print("Krishna Bhatia 1/23/SET/BCS/358")
