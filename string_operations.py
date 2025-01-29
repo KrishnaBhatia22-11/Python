1afe7931db24650d38d04fb105e68ad9c5e5ee09
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-29 19:23:02 +0530

    Create string_operations.py

---
+++ string_operations.py
@@ -0,0 +1,9 @@
+# 1. String Operations
+string1 = "Amazing"
+print("Complete String:", string1)
+print("First character:", string1[0])
+print("Characters from 3rd to 5th:", string1[2:5])
+print("String starting from 3rd character:", string1[2:])
+print("String two times:", string1 * 2)
+print("Concatenate 'Test' with 'Amazing':", "Test" + string1)
+print("String[-7:-3]:", string1[-7:-3])
