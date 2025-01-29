da4983612eb38e452bcb99a03e055ea55fa4bda6
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-29 19:27:36 +0530

    Create data_types_values.py

---
+++ data_types_values.py
@@ -0,0 +1,13 @@
+a = 2
+b = 4.5
+c = 3 + 2j
+
+print("THE DATA TYPE OF A IS ->", type(a))
+print("THE DATA TYPE OF B IS ->", type(b))
+print("THE DATA TYPE OF C IS ->", type(c))
+
+print("The value of a is:", a)
+print("The value of b is:", b)
+print("The value of c is:", c)
+
+print("Krishna Bhatia 1/23/SET/BCS/358")
