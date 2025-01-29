968c45be45dbdb072be2bcfa58c64c1c8b664050
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-29 19:24:32 +0530

    Create string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,8 @@
+string = input("Enter a string: ")
+print("Uppercase:", string.upper())
+print("Lowercase:", string.lower())
+print("Split:", string.split())
+print("Count of 'a':", string.count('a'))
+print("Replace 'a' with 'e':", string.replace('a', 'e'))
+print("Find 'test':", string.find('test'))
+print("Krishna Bhatia 1/23/SET/BCS/358")
