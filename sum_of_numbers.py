304f1ed542790a5d4572d593b9e9649a8c5b6903
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-30 10:53:46 +0530

    Create  sum_of_numbers.py

---
+++ sum_of_numbers.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+c = int(input("Enter the value of c:"))
+total=a+b+c
+if a==b==c:
+    total*=3
+print("The result is:",total)
+print("Krishna Bhatia 1/23/SET/BCS/358")
