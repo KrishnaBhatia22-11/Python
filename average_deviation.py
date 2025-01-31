5fb580b69fcb56279ca58c3028727afeeda3f68b
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-01-31 21:31:59 +0530

    Create average_deviation.py

---
+++ average_deviation.py
@@ -0,0 +1,15 @@
+# Accept inputs
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate average
+avg = (num1 + num2) / 2
+
+# Calculate deviation
+dev1 = num1 - avg
+dev2 = num2 - avg
+
+# Print results
+print("Average:", avg)
+print("Deviation of first number:", dev1)
+print("Deviation of second number:", dev2)
