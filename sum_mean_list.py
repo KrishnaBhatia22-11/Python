e4e2fe64507cb85e0a0018a87d70dab60f66297c
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-02-01 10:41:13 +0530

    Create sum_mean_list.py

---
+++ sum_mean_list.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5]
+total = 0
+
+for num in numbers:
+    total += num
+
+mean = total / len(numbers)
+
+print("Sum:", total)
+print("Mean:", mean)
+
+print("Krishna Bhatia 1/23/SET/BCS/358")
