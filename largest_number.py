97065d6552fa3d17f62a37154661e3f346351437
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-02-01 10:43:56 +0530

    Create largest_number.py

---
+++ largest_number.py
@@ -0,0 +1,9 @@
+num_list = [10, 50, 30, 90, 20]
+largest = num_list[0]
+
+for num in num_list:
+    if num > largest:
+        largest = num
+
+print("Largest number:", largest)
+print("Krishna Bhatia 1/23/SET/BCS/358")
