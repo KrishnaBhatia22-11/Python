fa0c99c388c8e1efe1ba1027b0e77d84c5c8f6ab
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-04-01 20:03:04 +0530

    Create numpy_operations.py

---
+++ numpy_operations.py
@@ -0,0 +1,8 @@
+arr1 = np.array([10, 20, 30])
+arr2 = np.array([1, 2, 3])
+
+print("Addition:", arr1 + arr2)
+print("Subtraction:", arr1 - arr2)
+print("Multiplication:", arr1 * arr2)
+print("Division:", arr1 / arr2)
+print("1/23/SET/BCS/358")
