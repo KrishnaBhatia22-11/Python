0f2c4ebe1ced1fc7332b3d294416aad67991b7b3
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-04-01 20:01:29 +0530

    Create numpy_slicing.py

---
+++ numpy_slicing.py
@@ -0,0 +1,6 @@
+arr = np.array([10, 20, 30, 40, 50, 60])
+
+print("Elements from index 1 to 4:", arr[1:5])
+print("Every second element:", arr[::2])
+print("Reverse order:", arr[::-1])
+print("1/23/SET/BCS/358")
