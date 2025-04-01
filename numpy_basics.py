27e435243d88d8f3aa1704e1723120d1a7bff9eb
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-04-01 20:00:30 +0530

    Create numpy_basics.py

---
+++ numpy_basics.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# 1D array
+arr1D = np.array([1, 2, 3, 4, 5])
+print("1D Array:\n", arr1D)
+
+# 2D array
+arr2D = np.array([[1, 2, 3], [4, 5, 6]])
+print("\n2D Array:\n", arr2D)
+
+# 3D array
+arr3D = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+print("\n3D Array:\n", arr3D)
+print("1/23/SET/BCS/358")
