d2a72910c9fe97efc8f55b08dc0ca4ac37e95774
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-04-01 20:03:38 +0530

    Create numpy_data_types.py

---
+++ numpy_data_types.py
@@ -0,0 +1,8 @@
+int_array = np.array([1, 2, 3])
+float_array = np.array([1.1, 2.2, 3.3])
+string_array = np.array(["a", "b", "c"])
+
+print("\nInteger Array Data Type:", int_array.dtype)
+print("Float Array Data Type:", float_array.dtype)
+print("String Array Data Type:", string_array.dtype)
+print("1/23/SET/BCS/358")
