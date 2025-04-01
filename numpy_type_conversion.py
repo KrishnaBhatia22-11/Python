5015ade23c2d504c5c7e5103c0b2518356718775
Krishna Bhatia <krishnabhatia971@gmail.com>
2025-04-01 20:04:31 +0530

    Create numpy_type_conversion.py

---
+++ numpy_type_conversion.py
@@ -0,0 +1,6 @@
+int_array = np.array([10, 20, 30])
+string_array = int_array.astype(str)
+
+print("\nString Array:", string_array)
+print("Data Type:", string_array.dtype)
+print("1/23/SET/BCS/358")
